017dc6f406159d3fd049eaaf35672b5eac9c3de4
Alireza Rastineh <kouroshrastineh95@gmail.com>
2023-06-12 12:13:59 +0200

    first commit - home page with products

---
+++ client/src/App.tsx
@@ -0,0 +1,46 @@
+import { Navbar, Container, Nav, Row, Col } from "react-bootstrap";
+import { sampleProducts } from "./data";
+
+function App() {
+  return (
+    <div className="d-flex flex-column vh-100">
+      <header>
+        <Navbar bg="dark" variant="dark" expand="lg">
+          <Container>
+            <Navbar.Brand>AlirezaBaba</Navbar.Brand>
+          </Container>
+          <Nav>
+            <a href="/cart" className="nav-link">
+              Cart
+            </a>
+            <a href="/signin" className="nav-link">
+              Sign In
+            </a>
+          </Nav>
+        </Navbar>
+      </header>
+      <main>
+        <Container className="mt-3">
+          <Row>
+            {sampleProducts.map((product) => (
+              <Col key={product.slug} sm={6} md={4} lg={3}>
+                <img
+                  src={product.image}
+                  alt={product.name}
+                  className="product-image"
+                />
+                <h2>{product.name}</h2>
+                <p>€{product.price}</p>
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      </main>
+      <footer>
+        <div className="text-center">All right reserved</div>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
