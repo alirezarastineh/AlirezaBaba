eddffff3aaa84a5774f727e1f19e476911cacb0c
Alireza Rastineh <kouroshrastineh95@gmail.com>
2023-06-14 14:36:47 +0200

    Create Place Order Page

--- client/src/Store.tsx
+++ client/src/Store.tsx
@@ -56,6 +56,7 @@ type Action =
   | { type: "SWITCH_MODE" }
   | { type: "CART_ADD_ITEM"; payload: CartItem }
   | { type: "CART_REMOVE_ITEM"; payload: CartItem }
+  | { type: "CART_CLEAR" }
   | { type: "USER_SIGNIN"; payload: UserInfo }
   | { type: "USER_SIGNOUT" }
   | { type: "SAVE_SHIPPING_ADDRESS"; payload: ShippingAddress }
@@ -94,6 +95,9 @@ function reducer(state: AppState, action: Action): AppState {
 
       return { ...state, cart: { ...state.cart, cartItems } };
     }
+
+    case "CART_CLEAR":
+      return { ...state, cart: { ...state.cart, cartItems: [] } };
 
     case "USER_SIGNIN":
       return { ...state, userInfo: action.payload };
