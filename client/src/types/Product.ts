2e24839767c11f2d293e1d2d0cca89de4130997d
Alireza Rastineh <kouroshrastineh95@gmail.com>
2023-06-16 17:13:42 +0200

    Adding Carousel To HomePage

--- client/src/types/Product.ts
+++ client/src/types/Product.ts
@@ -1,13 +1,24 @@
 export type Product = {
   _id: string;
   name: string;
-  slug: string;
   image: string;
+  banner: string;
   category: string;
   brand: string;
-  price: number;
-  countInStock: number;
   description: string;
+  reviews: Review[];
+  slug: string;
+  images: string[];
   rating: number;
   numReviews: number;
+  price: number;
+  countInStock: number;
+  isFeatured: boolean;
+};
+export type Review = {
+  _id: string;
+  createdAt: string;
+  name: string;
+  rating: number;
+  comment: string;
 };
