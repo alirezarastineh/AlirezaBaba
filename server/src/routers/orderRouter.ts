9dd2ff1042926ddd226ab1a987368334b8d47741
Alireza Rastineh <kouroshrastineh95@gmail.com>
2023-06-16 16:55:20 +0200

    adding new features for ordering

--- server/src/routers/orderRouter.ts
+++ server/src/routers/orderRouter.ts
@@ -1,10 +1,65 @@
 import express, { Request, Response } from "express";
 import asyncHandler from "express-async-handler";
+import { UserModel } from "../models/userModel";
 import { Order, OrderModel } from "../models/orderModel";
-import { Product } from "../models/productModel";
-import { isAuth } from "../utils";
+import { Product, ProductModel } from "../models/productModel";
+import { isAdmin, isAuth } from "../utils";
 
 export const orderRouter = express.Router();
+
+orderRouter.get(
+  "/",
+  isAuth,
+  isAdmin,
+  asyncHandler(async (req: Request, res: Response) => {
+    const orders = await OrderModel.find().populate("user", "name");
+    res.json(orders);
+  })
+);
+
+orderRouter.get(
+  "/summary",
+  isAuth,
+  isAdmin,
+  asyncHandler(async (req: Request, res: Response) => {
+    const orders = await OrderModel.aggregate([
+      {
+        $group: {
+          _id: null,
+          numOrders: { $sum: 1 },
+          totalSales: { $sum: "$totalPrice" },
+        },
+      },
+    ]);
+    const users = await UserModel.aggregate([
+      {
+        $group: {
+          _id: null,
+          numUsers: { $sum: 1 },
+        },
+      },
+    ]);
+    const dailyOrders = await OrderModel.aggregate([
+      {
+        $group: {
+          _id: { $dateToString: { format: "%Y-%m-%d", date: "$createdAt" } },
+          orders: { $sum: 1 },
+          sales: { $sum: "$totalPrice" },
+        },
+      },
+      { $sort: { _id: 1 } },
+    ]);
+    const productCategories = await ProductModel.aggregate([
+      {
+        $group: {
+          _id: "$category",
+          count: { $sum: 1 },
+        },
+      },
+    ]);
+    res.json({ users, orders, dailyOrders, productCategories });
+  })
+);
 
 orderRouter.get(
   "/mine",
@@ -12,20 +67,6 @@ orderRouter.get(
   asyncHandler(async (req: Request, res: Response) => {
     const orders = await OrderModel.find({ user: req.user._id });
     res.json(orders);
-  })
-);
-
-orderRouter.get(
-  // /api/orders/:id
-  "/:id",
-  isAuth,
-  asyncHandler(async (req: Request, res: Response) => {
-    const order = await OrderModel.findById(req.params.id);
-    if (order) {
-      res.json(order);
-    } else {
-      res.status(404).json({ message: "Order Not Found" });
-    }
   })
 );
 
@@ -54,11 +95,25 @@ orderRouter.post(
   })
 );
 
+orderRouter.get(
+  // /api/orders/:id
+  "/:id",
+  isAuth,
+  asyncHandler(async (req: Request, res: Response) => {
+    const order = await OrderModel.findById(req.params.id);
+    if (order) {
+      res.json(order);
+    } else {
+      res.status(404).json({ message: "Order Not Found" });
+    }
+  })
+);
+
 orderRouter.put(
   "/:id/pay",
   isAuth,
   asyncHandler(async (req: Request, res: Response) => {
-    const order = await OrderModel.findById(req.params.id);
+    const order = await OrderModel.findById(req.params.id).populate("user");
 
     if (order) {
       order.isPaid = true;
@@ -71,7 +126,41 @@ orderRouter.put(
       };
       const updatedOrder = await order.save();
 
-      res.send({ order: updatedOrder, message: "Order Paid Successfully" });
+      res.json({ order: updatedOrder, message: "Order Paid Successfully" });
+    } else {
+      res.status(404).json({ message: "Order Not Found" });
+    }
+  })
+);
+
+orderRouter.delete(
+  "/:id",
+  isAuth,
+  isAdmin,
+  asyncHandler(async (req: Request, res: Response) => {
+    const order = await OrderModel.findById(req.params.id);
+    if (order) {
+      const deleteOrder = await order.deleteOne();
+      res.json({ message: "Order Deleted", order: deleteOrder });
+    } else {
+      res.status(404).json({ message: "Order Not Found" });
+    }
+  })
+);
+
+orderRouter.put(
+  "/:id/deliver",
+  isAuth,
+  isAdmin,
+  asyncHandler(async (req: Request, res: Response) => {
+    const order = await OrderModel.findById(req.params.id);
+    if (order) {
+      order.isDelivered = true;
+      order.deliveredAt = new Date(Date.now());
+      // order.deliveredAt = Date.now();
+
+      const updatedOrder = await order.save();
+      res.json({ message: "Order Delivered", order: updatedOrder });
     } else {
       res.status(404).json({ message: "Order Not Found" });
     }
