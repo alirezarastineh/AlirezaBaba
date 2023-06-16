0e6bba5bf79599478e3ba77b284919a10d3f818b
Alireza Rastineh <kouroshrastineh95@gmail.com>
2023-06-16 16:57:21 +0200

    new features for users

--- server/src/routers/userRouter.ts
+++ server/src/routers/userRouter.ts
@@ -2,9 +2,29 @@ import express, { Request, Response } from "express";
 import asyncHandler from "express-async-handler";
 import bcrypt from "bcryptjs";
 import { User, UserModel } from "../models/userModel";
-import { generateToken, isAuth } from "../utils";
+import { generateToken, isAuth, isAdmin } from "../utils";
 
 export const userRouter = express.Router();
+
+userRouter.get("/:id", async (req, res) => {
+  const user = await UserModel.findById(req.params.id);
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404).json({ message: "User Not Found" });
+  }
+});
+
+userRouter.get(
+  "/",
+  isAuth,
+  isAdmin,
+  asyncHandler(async (req: Request, res: Response) => {
+    const users = await UserModel.find({});
+    res.json(users);
+  })
+);
+
 // POST /api/users/signin
 userRouter.post(
   "/signin",
@@ -56,7 +76,7 @@ userRouter.put(
         user.password = bcrypt.hashSync(req.body.password, 8);
       }
       const updatedUser = await user.save();
-      res.send({
+      res.json({
         _id: updatedUser._id,
         name: updatedUser.name,
         email: updatedUser.email,
@@ -67,5 +87,42 @@ userRouter.put(
     }
 
     res.status(404).json({ message: "User not found" });
+  })
+);
+
+userRouter.put(
+  "/:id",
+  isAuth,
+  isAdmin,
+  asyncHandler(async (req: Request, res: Response) => {
+    const user = await UserModel.findById(req.params.id);
+    if (user) {
+      user.name = req.body.name || user.name;
+      user.email = req.body.email || user.email;
+      user.isAdmin = Boolean(req.body.isAdmin);
+      const updatedUser = await user.save();
+      res.json({ message: "User Updated", user: updatedUser });
+    } else {
+      res.status(404).json({ message: "User Not Found" });
+    }
+  })
+);
+
+userRouter.delete(
+  "/:id",
+  isAuth,
+  isAdmin,
+  asyncHandler(async (req: Request, res: Response) => {
+    const user = await UserModel.findById(req.params.id);
+    if (user) {
+      if (user.email === "admin@example.com") {
+        res.status(400).json({ message: "Can Not Delete Admin User" });
+        return;
+      }
+      const deleteUser = await user.deleteOne();
+      res.json({ message: "User Deleted", user: deleteUser });
+    } else {
+      res.status(404).json({ message: "User Not Found" });
+    }
   })
 );
