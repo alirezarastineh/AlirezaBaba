7cb671b45e7384f6c83e774174393452b0a2f69a
Alireza Rastineh <kouroshrastineh95@gmail.com>
2023-06-17 13:16:35 +0200

    formats corrected

--- client/src/pages/ShippingAddressPage.tsx
+++ client/src/pages/ShippingAddressPage.tsx
@@ -58,7 +58,7 @@ export default function ShippingAddressPage() {
       <Helmet>
         <title>Shipping Address</title>
       </Helmet>
-      <CheckoutSteps step1 step2></CheckoutSteps>
+      <CheckoutSteps step1 step2 />
       <div className="container small-container">
         <h1 className="my-3">Shipping Address</h1>
         <Form onSubmit={submitHandler}>
