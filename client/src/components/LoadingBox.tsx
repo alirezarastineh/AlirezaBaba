c0f0ae51da99446d5c357b5f0df331846eed78a2
Alireza Rastineh <kouroshrastineh95@gmail.com>
2023-06-18 15:16:02 +0200

    new Loading

--- client/src/components/LoadingBox.tsx
+++ client/src/components/LoadingBox.tsx
@@ -1,9 +1,42 @@
-import { Spinner } from "react-bootstrap";
+import React, { useEffect, useRef } from "react";
+import lottie, { AnimationItem } from "lottie-web";
 
-export default function LoadingBox() {
-  return (
-    <Spinner animation="border" role="status">
-      <span className="visually-hidden">Loading</span>
-    </Spinner>
-  );
-}
+const LoadingBox: React.FC = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const animationRef = useRef<AnimationItem | null>(null);
+
+  useEffect(() => {
+    const fetchAnimationData = async () => {
+      try {
+        const response = await fetch(
+          "https://assets7.lottiefiles.com/packages/lf20_uqs1ib9v.json"
+        );
+        const animationData = await response.json();
+
+        if (containerRef.current) {
+          animationRef.current = lottie.loadAnimation({
+            container: containerRef.current,
+            renderer: "svg",
+            loop: true,
+            autoplay: true,
+            animationData: animationData,
+          });
+        }
+      } catch (error) {
+        console.error("Failed to fetch animation data:", error);
+      }
+    };
+
+    fetchAnimationData();
+
+    return () => {
+      if (animationRef.current) {
+        animationRef.current.destroy();
+      }
+    };
+  }, []);
+
+  return <div ref={containerRef} style={{ width: "100%", height: "100%" }} />;
+};
+
+export default LoadingBox;
