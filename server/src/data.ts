e3caec789830cf617704b586f8c7428d5aa6c7f0
Alireza Rastineh <kouroshrastineh95@gmail.com>
2023-07-10 12:24:29 +0200

    change the src for img

--- server/src/data.ts
+++ server/src/data.ts
@@ -6,7 +6,8 @@ export const sampleProducts: Product[] = [
   {
     name: "Lamborghini Huracán",
     slug: "lamborghini-huracán",
-    image: "/uploads/p4.jpg",
+    image:
+      "https://res.cloudinary.com/kouroshrstn/image/upload/v1688980649/uploads/p4.jpg",
     category: "Sport Car",
     brand: "Lamborghini",
     price: 200000.0,
@@ -17,14 +18,18 @@ export const sampleProducts: Product[] = [
     reviews: [
       { name: "John", comment: "good", rating: 4, createdAt: new Date() },
     ],
-    images: ["/uploads/p11.jpg"],
+    images: [
+      "https://res.cloudinary.com/kouroshrstn/image/upload/v1688980638/uploads/p11.jpg",
+    ],
     isFeatured: true,
-    banner: "/uploads/b1.jpg",
+    banner:
+      "https://res.cloudinary.com/kouroshrstn/image/upload/v1688980296/uploads/b1.jpg",
   },
   {
     name: "Lamborghini Terzo Millennio",
     slug: "lamborghini-terzo-millennio",
-    image: "/uploads/p3.jpg",
+    image:
+      "https://res.cloudinary.com/kouroshrstn/image/upload/v1688980643/uploads/p3.jpg",
     category: "Sport Car",
     brand: "Lamborghini",
     price: 2288000.0,
@@ -35,12 +40,14 @@ export const sampleProducts: Product[] = [
     reviews: [],
     images: [],
     isFeatured: true,
-    banner: "/uploads/b2.jpg",
+    banner:
+      "https://res.cloudinary.com/kouroshrstn/image/upload/v1688980283/uploads/b2.jpg",
   },
   {
     name: "Mercedes-Benz G-Class",
     slug: "mercedes-benz-g-class",
-    image: "/uploads/p2.jpg",
+    image:
+      "https://res.cloudinary.com/kouroshrstn/image/upload/v1688980295/uploads/p2.jpg",
     category: "Car",
     brand: "Mercedes-Benz",
     price: 155000.0,
@@ -55,7 +62,8 @@ export const sampleProducts: Product[] = [
   {
     name: "Dodge Challenger SRT Hellcat",
     slug: "dodge-challenger-srt-hellcat",
-    image: "/uploads/p1.jpg",
+    image:
+      "https://res.cloudinary.com/kouroshrstn/image/upload/v1688980296/uploads/p1.jpg",
     category: "Car",
     brand: "Dodge",
     price: 92000.0,
