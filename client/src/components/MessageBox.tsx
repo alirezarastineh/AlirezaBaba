dac65b7097bd965b4540fc542e0b29d4a7233163
Alireza Rastineh <kouroshrastineh95@gmail.com>
2023-07-12 11:20:11 +0200

    deleting some comments

--- client/src/components/MessageBox.tsx
+++ client/src/components/MessageBox.tsx
@@ -8,6 +8,5 @@ export default function MessageBox({
   variant?: string;
   children: React.ReactNode;
 }) {
-  // Renders an Alert with a variant and children passed as props
   return <Alert variant={variant || "info"}>{children}</Alert>;
 }
