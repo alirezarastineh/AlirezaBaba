46eef86e2bbf596532f4741d4362a4413e38c7d7
Alireza Rastineh <kouroshrastineh95@gmail.com>
2023-07-12 11:20:24 +0200

    deleting some comments

--- client/src/components/ProtectedRoute.tsx
+++ client/src/components/ProtectedRoute.tsx
@@ -3,15 +3,9 @@ import { Navigate, Outlet } from "react-router-dom";
 import { Store } from "../Store";
 
 export default function ProtectedRoute() {
-  // Access the global state from the Store context
   const {
     state: { userInfo },
   } = useContext(Store);
 
-  // If the userInfo object exists, the Outlet component (which renders the child route component) is returned
-  // If the userInfo object does not exist, they are redirected to the Sign In Page
   return userInfo ? <Outlet /> : <Navigate to="/signin" />;
 }
-
-// The <Outlet /> component is a placeholder that represents the nested child routes within a parent route.
-// It allows the child routes to be rendered when the parent route matches.
