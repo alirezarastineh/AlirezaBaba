6760bc67709f83d4a5eedcb434771ce4946c63e3
Alireza Rastineh <kouroshrastineh95@gmail.com>
2023-07-12 11:20:45 +0200

    deleting some comments

--- client/src/components/Products/ProductItem.tsx
+++ client/src/components/Products/ProductItem.tsx
@@ -9,24 +9,20 @@ import { convertProductToCartItem } from "../../utils";
 import Rating from "./Rating";
 
 function ProductItem({ product }: { product: Product }) {
-  // Access the global state and dispatch function from the Store context
   const { state, dispatch } = useContext(Store);
   const {
-    cart: { cartItems }, // Destructure the cartItems array from the global state
+    cart: { cartItems },
   } = state;
 
   const addToCartHandler = (item: CartItem) => {
-    // Check if the product already exists in the cart
     const existItem = cartItems.find((x) => x._id === product._id);
-    const quantity = existItem ? existItem.quantity + 1 : 1; // Increase the quantity if the product already exists
+    const quantity = existItem ? existItem.quantity + 1 : 1;
 
-    // Check if the quantity exceeds the available stock
     if (product.countInStock < quantity) {
       alert("Sorry. Product is out of stock");
       return;
     }
 
-    // Dispatch an action to add the item to the cart in the global state
     dispatch({
       type: "CART_ADD_ITEM",
       payload: { ...item, quantity },
@@ -36,7 +32,6 @@ function ProductItem({ product }: { product: Product }) {
 
   return (
     <Card>
-      {/* Link to the product details page */}
       <Link to={`/product/${product.slug}`}>
         <img
           src={product.image}
@@ -48,10 +43,8 @@ function ProductItem({ product }: { product: Product }) {
         <Link to={`/product/${product.slug}`}>
           <Card.Title>{product.name}</Card.Title>
         </Link>
-        {/* Display the product rating */}
         <Rating rating={product.rating} numReviews={product.numReviews} />
         <Card.Text>€{product.price}</Card.Text>
-        {/* Display a button based on the product's availability */}
         {product.countInStock === 0 ? (
           <Button variant="light" disabled>
             Out of stock
