65fb97bf4c0db22a317f63926b60da15a1260a59
Alireza Rastineh <kouroshrastineh95@gmail.com>
2023-07-12 15:30:38 +0200

    rearranging imports

--- client/src/pages/Admin/Products/ProductEditPage.tsx
+++ client/src/pages/Admin/Products/ProductEditPage.tsx
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { toast } from "react-toastify";
-import { Container, ListGroup, Button } from "react-bootstrap";
-import Form from "react-bootstrap/Form";
+import { Container, ListGroup, Button, Form } from "react-bootstrap";
 import { Helmet } from "react-helmet-async";
 import { getError } from "../../../utils";
 import LoadingBox from "../../../components/LoadingBox";
