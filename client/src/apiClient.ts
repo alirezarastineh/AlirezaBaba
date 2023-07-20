02ddf5d7760407e76f5ef3dc862365b433645c2e
Alireza Rastineh <kouroshrastineh95@gmail.com>
2023-07-21 00:31:43 +0200

    remote changes

--- client/src/apiClient.ts
+++ client/src/apiClient.ts
@@ -2,7 +2,9 @@ import axios from "axios";
 
 const apiClient = axios.create({
   baseURL:
-    process.env.NODE_ENV === "development" ? "http://localhost:4000/" : "/",
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:4000/"
+      : "https://alireza-baba-server.vercel.app/",
   headers: {
     "Content-type": "application/json",
   },
