f5ac3602cc5dfb21271e20247935f741c278457a
Alireza Rastineh <kouroshrastineh95@gmail.com>
2023-07-21 00:35:33 +0200

    remove changes

--- client/src/apiClient.ts
+++ client/src/apiClient.ts
@@ -2,9 +2,7 @@ import axios from "axios";
 
 const apiClient = axios.create({
   baseURL:
-    process.env.NODE_ENV === "development"
-      ? "http://localhost:4000/"
-      : "https://alireza-baba-server.vercel.app/",
+    process.env.NODE_ENV === "development" ? "http://localhost:4000/" : "/",
   headers: {
     "Content-type": "application/json",
   },
