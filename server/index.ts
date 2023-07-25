33b7fb3d30fececf1b7a21a78323f278894ec3fb
Alireza Rastineh <kouroshrastineh95@gmail.com>
2023-07-25 13:38:52 +0200

    index moved to root folder

--- server/index.ts
+++ server/index.ts
@@ -2,12 +2,12 @@ import cors from "cors";
 import dotenv from "dotenv";
 import express, { NextFunction, Request, Response } from "express";
 import mongoose from "mongoose";
-import { keyRouter } from "./routers/keyRouter";
-import { orderRouter } from "./routers/orderRouter";
-import { productRouter } from "./routers/productRouter";
-import { seedRouter } from "./routers/seedRouter";
-import { userRouter } from "./routers/userRouter";
-import { uploadRouter } from "./routers/uploadRouter";
+import { keyRouter } from "./src/routers/keyRouter";
+import { orderRouter } from "./src/routers/orderRouter";
+import { productRouter } from "./src/routers/productRouter";
+import { seedRouter } from "./src/routers/seedRouter";
+import { userRouter } from "./src/routers/userRouter";
+import { uploadRouter } from "./src/routers/uploadRouter";
 
 dotenv.config();
 
